86f9f86eee6e1f6a7c209e348d8a280f375dab18
SophiaNM <njerimurage92@outlook.com>
2018-06-24 21:18:15 +0300

    Updating users class for authentication

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 # File for models/classes
 from . import db
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__='users'
@@ -11,26 +11,23 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    # password_hash = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     #
     # reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
-    # # pass_secure =db.Column(db.String(255))
     #
-    # @property
-    # def password(self):
-    #     raise AttributeError('You Cannot read the password attribute')
-    #
-    # @password.setter
-    # def password(self,password):
-    #     self.password_hash = generate_password_hash(password)
-    #
-    #
-    #
-    # def verify_password(self,password):
-    #     return check_password_hash(self.password_hash,password)
-    #
-    # def __repr__(self):
-    #     return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You Cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 
