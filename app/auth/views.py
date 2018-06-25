8c66f0b71aea3d65406aae7625e4db3900ab5dc6
SophiaNM <njerimurage92@outlook.com>
2018-06-25 10:30:44 +0300

    Defining login an dregister view functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,46 @@
-from flask import render_template
+from flask import render_template, redirect,url_for, flash, request
 from . import auth
+from ..models import User
+from .. import db
+from flask_login import login_user,logout_user,login_required
+from .forms import LoginForm,RegistrationForm
+# from ..email import mail_message
 
 
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET','POST'])
 def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email =login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+        flash('Invalid username or Password')
 
-    title = 'Login | One Min Pitch'
-    # form = LoginForm()
+    title = "login | One Min Pitch"
+    return render_template('auth/login.html', login_form = login_form ,title=title)
 
-    return render_template('auth/login.html', title = title )
 
-@auth.route('/register')
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
+
+@auth.route('/register',methods = ["GET","POST"])
 def register():
 
     '''
     View root page function that returns the index page and its data
     '''
-
-    title = 'Register | One Min Pitch'
-    # form = RegistrationForm()
-
-    return render_template('auth/login.html', title = title )
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account | One Min Pitch"
+    return render_template('auth/register.html',registration_form = form)
