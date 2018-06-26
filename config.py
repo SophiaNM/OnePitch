fc67540419a0655ab25366c7579b75bc058ed0f1
SophiaNM <njerimurage92@outlook.com>
2018-06-26 09:47:38 +0300

    Test case

--- config.py
+++ config.py
@@ -4,10 +4,15 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:sophia@localhost/onepitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 class ProdConfig(Config):
     '''
@@ -25,7 +30,8 @@ class TestConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:sophia@localhost/onepitch_test'
+
 
 class DevConfig(Config):
     '''
@@ -34,7 +40,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njeri:sophia@localhost/onepitch'
     DEBUG = True
 
 
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 # Connect to models
-from app.models import User,Role
+from app.models import User,Role,Pitch
 # Set up migrations
 from flask_migrate import Migrate,MigrateCommand
 
@@ -33,7 +33,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict( app=app, db=db, User=User, Role=Role)
+    return dict( app=app, db=db, User=User, Role=Role, Pitch=Pitch)
 
 
 if __name__ == '__main__':
