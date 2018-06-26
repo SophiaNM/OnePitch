0603c0e3c0613fca52ce54b7f8a52171e5904e16
SophiaNM <njerimurage92@outlook.com>
2018-06-26 17:26:16 +0300

    Defining pitch class model

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .forms import UpdateProfile, PitchForm
-from ..models import User, Role, Pitch
+from .forms import UpdateProfile, PitchForm, CommentForm
+from ..models import User, Role, Pitch, Comment
 from flask_login import login_required, current_user
 from .. import db, photos
 
@@ -16,8 +16,9 @@ def index():
     productpitches = Pitch.query.filter_by(category="Product-Pitch").order_by(Pitch.posted.desc()).all()
     promotionpitches = Pitch.query.filter_by(category="Promotion-Pitch").order_by(Pitch.posted.desc()).all()
     businesspitches = Pitch.query.filter_by(category="Business-Pitch").order_by(Pitch.posted.desc()).all()
-    # all_pitches = Pitch.get_all_pitches()
+
     pitch = Pitch.get_all_pitches()
+
     # print(all_pitches)
 
     title = 'Home | One Min Pitch'
@@ -27,6 +28,9 @@ def index():
 
 @main.route('/user/<uname>')
 def profile(uname):
+    '''
+    View profile page function that returns the profile page and its data
+    '''
     user = User.query.filter_by(username = uname).first()
 
     if user is None:
@@ -34,9 +38,13 @@ def profile(uname):
 
     return render_template("profile/profile.html", user = user)
 
+
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
+    '''
+    View update profile page function that returns the update profile page and its data
+    '''
     user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
@@ -53,9 +61,13 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form =form)
 
+
 @main.route('/user/<uname>/update/pic',methods=['POST'])
 @login_required
 def update_pic(uname):
+    '''
+    View update pic profile function that returns the uppdate profile pic page
+    '''
     user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
@@ -65,38 +77,13 @@ def update_pic(uname):
     return redirect(url_for('main.profile', uname=uname))
 
 
-@main.route('/comment')
-@login_required
-def comment():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    title = 'Home'
-
-    return render_template('comment.html', title = title )
-
-
-
-
 
 @main.route('/home', methods = ['GET', 'POST'])
 @login_required
 def home():
-    # pitch_form = PitchForm()
-    # pitch = get_pitch(id)
-    # my_pitch = Pitch.query.get(id)
-
-    # if pitch_form.validate_on_submit():
-    #     pitch = pitch_form.pitch.data
-    #     category = pitch_form.my_category.data
-    #
-    #     new_pitch = Pitch(pitch_desc=pitch, pitch_category = category, user = current_user)
-    #     new_pitch.save_pitch()
-    #
-    #     return redirect(url_for('main.home'))
-    # user = User.query.filter_by()
+    '''
+    View home function that returns the home page
+    '''
     interviewpitches = Pitch.query.filter_by(category="Interview-Pitch").order_by(Pitch.posted.desc()).all()
     productpitches = Pitch.query.filter_by(category="Product-Pitch").order_by(Pitch.posted.desc()).all()
     promotionpitches = Pitch.query.filter_by(category="Promotion-Pitch").order_by(Pitch.posted.desc()).all()
@@ -112,6 +99,9 @@ def home():
 @main.route('/pitch/new',methods = ['GET','POST'])
 @login_required
 def pitch():
+    '''
+    View pitch function that returns the pitch page and data
+    '''
     pitch_form = PitchForm()
 
     if pitch_form.validate_on_submit():
@@ -126,3 +116,32 @@ def pitch():
 
     title = 'New Pitch | One Minute Pitch'
     return render_template('pitch.html', title = title, pitch_form = pitch_form,)
+
+
+@main.route('/pitch/<int:pitch_id>/comment',methods = ['GET', 'POST'])
+@login_required
+def comment(pitch_id):
+    '''
+    View comments page function that returns the comment page and its data
+    '''
+    # comment_form = CommentForm()
+
+    comment_form = CommentForm()
+    my_pitch = Pitch.query.get(pitch_id)
+    if my_pitch is None:
+        abort(404)
+
+    if comment_form.validate_on_submit():
+        comment_content = comment_form.comment_content.data
+
+        new_comment = Comment(comment_content=comment_content, pitch_id = pitch_id, user = current_user)
+        new_comment.save_comment()
+
+        return redirect(url_for('.comment', pitch_id=pitch_id))
+
+    all_comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    # all_comments = Comment.get_all_comments(id)
+    # all_comments = Comment.get_all_comments(pitch_id)
+    title = 'New Comment | One Min Pitch'
+
+    return render_template('comment.html', title = title, pitch=my_pitch ,comment_form = comment_form, comment = all_comments )
