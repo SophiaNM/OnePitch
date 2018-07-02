4a28f949351a7fea98053e2a055cb8c52ff935c8
SophiaNM <njerimurage92@outlook.com>
2018-07-02 18:56:03 +0300

    hosting pictures

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 export SECRET_KEY='f48a3794b5f1a7b6d359b09ec83334ce'
 export MAIL_USERNAME='smurage.dev@gmail.com'
 export MAIL_PASSWORD='sobayeni'
-export DATABASE_URL='postgres://fzuzomztiqcxun:56572ed581882d002c876f385e824aad57b0cbe6081456e2511f1940ceff3ecb@ec2-23-21-236-249.compute-1.amazonaws.com:5432/dfaht87buk59i5'
-
+export DATABASE_URL='postgres://pcpmqnpoucwowj:fff717fd9dd55d88311c4c1bb139242a300c7a664a42b8147fd40d5614e6e5e0@ec2-174-129-22-84.compute-1.amazonaws.com:5432/d1gv00slncel9h'
 
 python3.6 manage.py server
