8eb0cc0a3d6db5ac1bcdc9f751bd495135659883
SophiaNM <njerimurage92@outlook.com>
2018-07-02 13:11:04 +0300

    Created tests for user and pitch class models

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import Pitch, User
+from app.models import Pitch, User, Comment
 from app import db
 
 
@@ -15,7 +15,8 @@ class PitchTest(unittest.TestCase):
         '''
 
         self.user_Sophy = User(username = 'Sophy', password = 'potato', email = 'sophy@ms.com')
-        self.new_pitch = Pitch(id=12345,pitch_title="First Pitch", content='Awesome pitch for stuff',category='Interview Pitch',author = self.user_Sophy)
+        self.new_comment = Comment(comment_content = 'Awesome stuff', pitch_id = 12345, user_id=self.user_Sophy)
+        self.new_pitch = Pitch(id=12345,pitch_title="First Pitch", content='Awesome pitch for stuff',category='Interview Pitch',author = self.user_Sophy,comments = self.new_comment)
 
     def tearDown(self):
         Pitch.query.delete()
@@ -27,6 +28,7 @@ class PitchTest(unittest.TestCase):
         self.assertEquals(self.new_pitch.content,'Awesome pitch for stuff')
         self.assertEquals(self.new_pitch.category,"Interview Pitch")
         self.assertEquals(self.new_pitch.author,self.user_Sophy)
+        self.assertEquals(self.new_pitch.comments,self.new_comment)
 
     def test_save_pitch(self):
         self.new_pitch.save_pitch()
