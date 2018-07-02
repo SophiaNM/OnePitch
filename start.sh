1ee04021f1ddcd06c27abc75312fb587ac0efd77
SophiaNM <njerimurage92@outlook.com>
2018-07-02 17:31:22 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='f48a3794b5f1a7b6d359b09ec83334ce'
 export MAIL_USERNAME='smurage.dev@gmail.com'
 export MAIL_PASSWORD='sobayeni'
-export DATABASE_URL='postgres://hdjwkbpewmfbvv:247a32093f1146df56a5809dd4fe666a185cd15dd2fd576e571280e7871e5a79@ec2-23-21-236-249.compute-1.amazonaws.com:5432/de3lu6b6rt2bmd'
+export DATABASE_URL='postgres://fzuzomztiqcxun:56572ed581882d002c876f385e824aad57b0cbe6081456e2511f1940ceff3ecb@ec2-23-21-236-249.compute-1.amazonaws.com:5432/dfaht87buk59i5'
 
 
 python3.6 manage.py server
