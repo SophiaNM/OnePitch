d4df76cdf82bebe24493cbc2828ba90b33052ee3
SophiaNM <njerimurage92@outlook.com>
2018-07-02 11:40:06 +0300

    Created like and dislike class models

--- app/models.py
+++ app/models.py
@@ -21,6 +21,9 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(255))
 
     pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+    likes = db.relationship('Like',backref = 'user',lazy = "dynamic")
+    dislikes = db.relationship('Dislike',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
@@ -64,6 +67,10 @@ class Pitch(db.Model):
 
     author = db.Column(db.Integer,db.ForeignKey("users.id"))
 
+    comments = db.relationship('Comment',backref = 'pitch',lazy = "dynamic")
+    likes = db.relationship('Like',backref = 'pitch',lazy = "dynamic")
+    dislikes = db.relationship('Dislike',backref = 'pitch',lazy = "dynamic")
+
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
@@ -86,4 +93,96 @@ class Pitch(db.Model):
 
 
     def __repr__(self):
-        return f'Pitch {self.pitch_desc}'
+        return f'Pitch {self.pitch_title}'
+
+
+
+
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key=True)
+    comment_content = db.Column(db.String())
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+    @classmethod
+    def get_all_comments(cls,id):
+        comments = Comment.query.order_by('-id').all()
+        return comments
+
+
+
+class Like (db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    like = db.Column(db.Integer,default=1)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_likes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_likes(cls,id):
+        like_pitch = Like(user = current_user, pitch_id=id)
+        like_pitch.save_likes()
+
+    @classmethod
+    def get_likes(cls,id):
+        like = Like.query.filter_by(pitch_id=id).all()
+        return like
+
+
+    @classmethod
+    def get_all_likes(cls,pitch_id):
+        likes = Like.query.order_by('-id').all()
+        return likes
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+
+
+class Dislike (db.Model):
+    __tablename__ = 'dislikes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    dislike = db.Column(db.Integer,default=1)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_dislikes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_dislikes(cls,id):
+        dislike_pitch = Dislike(user = current_user, pitch_id=id)
+        dislike_pitch.save_dislikes()
+
+    @classmethod
+    def get_dislikes(cls,id):
+        dislike = Dislike.query.filter_by(pitch_id=id).all()
+        return dislike
+
+
+    @classmethod
+    def get_all_dislikes(cls,pitch_id):
+        dislikes = Dislike.query.order_by('-id').all()
+        return dislikes
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
